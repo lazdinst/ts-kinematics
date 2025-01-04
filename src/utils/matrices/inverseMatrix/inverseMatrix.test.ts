f8c0494205e7a7592928f208563cbfe8d46a3730
Talis Lazdins <lazdinst@gmail.com>
2025-01-04 10:21:17 -0500

    Added test case for validation.

--- src/utils/matrices/inverseMatrix/inverseMatrix.test.ts
+++ src/utils/matrices/inverseMatrix/inverseMatrix.test.ts
@@ -1,8 +1,8 @@
 import { inverseMatrix } from "./inverseMatrix";
-// import {}
+import { Matrix } from "../../../definitions";
 describe("inverseMatrix", () => {
-  it("computes the inverse of a square matrix", () => {
-    const matrix = [
+  it("computes the inverse of a 2x2 square matrix", () => {
+    const matrix: Matrix = [
       [4, 7],
       [2, 6],
     ];
@@ -11,6 +11,21 @@ describe("inverseMatrix", () => {
     expect(result).toEqual([
       [0.6, -0.7],
       [-0.2, 0.4],
+    ]);
+  });
+
+  it("computes the inverse of a 3x3 square matrix", () => {
+    const matrix: Matrix = [
+      [2, 1, 1],
+      [4, -6, 0],
+      [-2, 7, 2],
+    ];
+    const result = inverseMatrix(matrix);
+
+    expect(result).toEqual([
+      [0.75, -0.3125, -0.375],
+      [0.5, -0.375, -0.25],
+      [-1.0, 1.0, 1.0],
     ]);
   });
 
